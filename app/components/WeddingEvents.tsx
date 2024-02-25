609b74dc8491ff18e19590115868c920f7e1ca44
Romireverend <rev4rt@gmail.com>
2024-02-26 06:57:56 +0700

    Update WeddingEvents.tsx
    ghgfh

--- app/components/WeddingEvents.tsx
+++ app/components/WeddingEvents.tsx
@@ -66,6 +66,10 @@ function WeddingEvents() {
                     LIVESTREAMING
                   </a>
                 </div>
+                 <div className="w-[20%] bg-[#424242] rounded-br-[1.25rem] flex items-center">
+                  <p className="-rotate-90 text-center text-white font-bold text-[1.75rem] -translate-x-[3rem] lg:-translate-x-[2.35rem] tracking-[5px]">
+                  </p>
+                </div>
               </div>
             </div>
           </RevealWrapper>
