aebf4fe72539c898f780b66500f87f68865b70fb
Romireverend <rev4rt@gmail.com>
2024-02-26 07:44:59 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ import ArRum from "./components/ArRum";
 import Hero from "./components/Hero";
 import Profile from "./components/Profile";
 import WeddingEvents from "./components/WeddingEvents";
-import Gallery from "./components/Gallery";
 import Footer from "./components/Footer";
 import { RevealWrapper } from "next-reveal";
 
@@ -27,7 +26,6 @@ export default function Home() {
       <ArRum />
       <Profile />
       <WeddingEvents />
-      <Gallery />
       <Footer />
     </main>
   );
