b5052e094d44818f6328f517da4273ec7bb11a6e
Romireverend <rev4rt@gmail.com>
2024-02-27 16:33:06 +0700

    Update WeddingEvents.tsx

--- app/components/WeddingEvents.tsx
+++ app/components/WeddingEvents.tsx
@@ -20,7 +20,48 @@ function WeddingEvents() {
               />
               <div className="flex">
                 <div className="w-[20%] bg-[#424242] rounded-bl-[1.25rem] flex items-center">
-                  <p className="rotate-90 text-center text-white font-bold text-[1rem] -translate-x-5 lg:-translate-x-3.5 tracking-[5px]">
+                  <p className="rotate-90 text-center text-white font-bold text-[1.75rem] -translate-x-5 lg:-translate-x-3.5 tracking-[5px]">
+                    PENGIRING
+                  </p>
+                </div>
+                <div className="w-[80%] bg-white px-4 py-6 rounded-br-[1.25rem] flex flex-col gap-5 items-baseline">
+                  <h1 className="italic text-xl font-light">
+                    Sunday, 21 April 2024
+                    <br />
+                    Ahad, 12-13 Syawal 1445
+                  </h1>
+                  <hr className="border border-[#5a5a5a80] w-full" />
+                  <p className="flex items-center text-[#5a5a5a] font-light text-sm gap-1.5">
+                    <AiFillClockCircle />
+                    <span>07.00 WIB - Selesai</span>
+                  </p>
+                  <p className="text-[0.785rem] text-[#5a5a5a] font-light leading-[1.9]">
+                    <strong className="text-black font-bold">
+                      Kediaman Mempelai Pria
+                    </strong>
+                    <br />
+                    Sajengan, Dusun Duwa' Assen, Batioh
+                  </p>
+                  <a
+                    href="https://maps.app.goo.gl/Nadp4o3ALL16yubp9"
+                    className="text-[0.75rem] text-[#424242] border border-[#424242] px-[0.4375rem] py-1.5 rounded-full hover:scale-90 ease-linear duration-[0.2s]"
+                  >
+                    GOOGLE MAPS
+                  </a>
+                </div>
+              </div>
+            </div>
+          </RevealWrapper>
+          <RevealWrapper duration={1500} origin="bottom">
+            <div className="mt-10">
+              <img
+                src="/images/weddingEvents/resepsi.jpg"
+                alt="akad"
+                className="w-full h-72 object-cover rounded-t-[1.25rem]"
+              />
+              <div className="flex">
+                <div className="w-[20%] bg-[#424242] rounded-bl-[1.25rem] flex items-center">
+                  <p className="-rotate-90 text-center text-white font-bold text-[1.75rem] -translate-x-[3rem] lg:-translate-x-[2.35rem] tracking-[5px]">
                     AKAD & RESEPSI
                   </p>
                 </div>
@@ -48,29 +89,6 @@ function WeddingEvents() {
                   >
                     GOOGLE MAPS
                   </a>
-                </div>
-              </div>
-            </div>
-          </RevealWrapper>
-          <RevealWrapper duration={1500} origin="bottom">
-            <div className="mt-6">
-              <img
-                src="/images/weddingEvents/resepsi.jpg"
-                alt="akad"
-                className="w-full h-72 object-cover rounded-t-[1.25rem] object-left"
-              />
-              <div className="flex">
-                <div className="w-[80%] bg-white px-4 py-6 rounded-bl-[1.25rem] flex flex-col gap-5 items-baseline">
-                  <a
-                    href="https://youtu.be/cRtC46Qajok?si=bxSj2QouUMzKJs7P"
-                    className="text-[0.75rem] text-[#424242] border border-[#424242] px-[0.4375rem] py-1.5 rounded-full hover:scale-90 ease-linear duration-[0.2s]"
-                  >
-                    LIVESTREAMING
-                  </a>
-                </div>
-                 <div className="w-[20%] bg-[#424242] rounded-br-[1.25rem] flex items-center">
-                  <p className="-rotate-90 text-center text-white font-bold text-[1.75rem] -translate-x-[3rem] lg:-translate-x-[2.35rem] tracking-[5px]">
-                  </p>
                 </div>
               </div>
             </div>
