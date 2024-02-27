3a72432729e717369c233078224113e222d2933a
Romireverend <rev4rt@gmail.com>
2024-02-28 03:28:32 +0700

    Update WeddingEvents.tsx

--- app/components/WeddingEvents.tsx
+++ app/components/WeddingEvents.tsx
@@ -107,8 +107,9 @@ function WeddingEvents() {
                 </div>
                 <div className="w-[80%] bg-white px-4 py-6 rounded-br-[1.25rem] flex flex-col gap-5 items-baseline">
                   <h1 className="italic text-xl font-light">
-                    AKAD & RESEPSI PERNIKAHAN AKHMAD MUHTAROM & ULIN NIKMAH
+                    Akad & Resepsi Pernikahan Akhmad Muhtarom & Ulin Nikmah
                   </h1>
+                  <hr className="border border-[#5a5a5a80] w-full" />
                   <a
                     href="https://facebook.com/RTPROJEX/live_videos"
                     className="text-[0.75rem] text-[#424242] border border-[#424242] px-[0.4375rem] py-1.5 rounded-full hover:scale-90 ease-linear duration-[0.2s]"
