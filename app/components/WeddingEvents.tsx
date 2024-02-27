ee31b8bf4aab2d79f88ffa238c348b31e1181979
Romireverend <rev4rt@gmail.com>
2024-02-27 16:41:56 +0700

    Update WeddingEvents.tsx

--- app/components/WeddingEvents.tsx
+++ app/components/WeddingEvents.tsx
@@ -20,30 +20,30 @@ function WeddingEvents() {
               />
               <div className="flex">
                 <div className="w-[20%] bg-[#424242] rounded-bl-[1.25rem] flex items-center">
-                  <p className="rotate-90 text-center text-white font-bold text-[1.75rem] -translate-x-5 lg:-translate-x-3.5 tracking-[5px]">
-                    PENGIRING
+                  <p className="rotate-90 text-center text-white font-bold text-[1.20rem] -translate-x-5 lg:-translate-x-3.5 tracking-[5px]">
+                    PENGIRING MEMPELAI PRIA
                   </p>
                 </div>
                 <div className="w-[80%] bg-white px-4 py-6 rounded-br-[1.25rem] flex flex-col gap-5 items-baseline">
                   <h1 className="italic text-xl font-light">
-                    Sunday, 21 April 2024
-                    <br />
-                    Ahad, 12-13 Syawal 1445
+                  Sunday, 21 April 2024
+                    </ br>
+                  Ahad, 12 Sayawal 1445
                   </h1>
                   <hr className="border border-[#5a5a5a80] w-full" />
                   <p className="flex items-center text-[#5a5a5a] font-light text-sm gap-1.5">
                     <AiFillClockCircle />
-                    <span>07.00 WIB - Selesai</span>
+                    <span>08.00 WIB</span>
                   </p>
                   <p className="text-[0.785rem] text-[#5a5a5a] font-light leading-[1.9]">
                     <strong className="text-black font-bold">
-                      Kediaman Mempelai Pria
+                      Kediaman Mempelai Putra
                     </strong>
                     <br />
-                    Sajengan, Dusun Duwa' Assen, Batioh
+                    Sajengan, Dusun Duwa' Assen, Batioh, Banyuates
                   </p>
                   <a
-                    href="https://maps.app.goo.gl/Nadp4o3ALL16yubp9"
+                    href="https://www.google.com/maps/place/Mercure+Bandung+City+Centre/@-6.9238326,107.608777,17z/data=!3m1!4b1!4m9!3m8!1s0x2e68e628c43b0159:0x7ad6a19680a00592!5m2!4m1!1i2!8m2!3d-6.9238326!4d107.6113519!16s%2Fg%2F11c5ggy5hx"
                     className="text-[0.75rem] text-[#424242] border border-[#424242] px-[0.4375rem] py-1.5 rounded-full hover:scale-90 ease-linear duration-[0.2s]"
                   >
                     GOOGLE MAPS
@@ -53,35 +53,32 @@ function WeddingEvents() {
             </div>
           </RevealWrapper>
           <RevealWrapper duration={1500} origin="bottom">
-            <div className="mt-10">
+            <div className="mt-6">
               <img
                 src="/images/weddingEvents/resepsi.jpg"
                 alt="akad"
-                className="w-full h-72 object-cover rounded-t-[1.25rem]"
+                className="w-full h-72 object-cover rounded-t-[1.25rem] object-left"
               />
               <div className="flex">
-                <div className="w-[20%] bg-[#424242] rounded-bl-[1.25rem] flex items-center">
-                  <p className="-rotate-90 text-center text-white font-bold text-[1.75rem] -translate-x-[3rem] lg:-translate-x-[2.35rem] tracking-[5px]">
-                    AKAD & RESEPSI
-                  </p>
-                </div>
-                <div className="w-[80%] bg-white px-4 py-6 rounded-br-[1.25rem] flex flex-col gap-5 items-baseline">
+                <div className="w-[80%] bg-white px-4 py-6 rounded-bl-[1.25rem] flex flex-col gap-5 items-baseline">
                   <h1 className="italic text-xl font-light">
                     Sunday, 21 April 2024
-                    <br />
-                    Ahad, 12-13 Syawal 1445
+                    </ br>
+                    Ahad, 12 Sayawal 1445
                   </h1>
                   <hr className="border border-[#5a5a5a80] w-full" />
-                  <p className="flex items-center text-[#5a5a5a] font-light text-sm gap-1.5">
-                    <AiFillClockCircle />
-                    <span>08.00 WIB - Selesai</span>
-                  </p>
+                  <div>
+                    <p className="flex items-center text-[#5a5a5a] font-light text-[0.8rem] gap-1.5 mt-1">
+                      <AiFillClockCircle />
+                      <span>08.00 WIB - SELESAI</span>
+                    </p>
+                  </div>
                   <p className="text-[0.785rem] text-[#5a5a5a] font-light leading-[1.9]">
                     <strong className="text-black font-bold">
                       Kediaman Mempelai Wanita
                     </strong>
                     <br />
-                    Dusun Duwa' Assen - Batioh
+                    Dusun Duwa' Assen, Batioh, Banyuates
                   </p>
                   <a
                     href="https://maps.app.goo.gl/Nadp4o3ALL16yubp9"
@@ -89,6 +86,11 @@ function WeddingEvents() {
                   >
                     GOOGLE MAPS
                   </a>
+                </div>
+                <div className="w-[20%] bg-[#424242] rounded-br-[1.25rem] flex items-center">
+                  <p className="-rotate-90 text-center text-white font-bold text-[1.20rem] -translate-x-[3rem] lg:-translate-x-[2.35rem] tracking-[5px]">
+                    AKAD & RESEPSI
+                  </p>
                 </div>
               </div>
             </div>
