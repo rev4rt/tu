687b5161f6e53ccec4689c62644a425884d20287
Romireverend <rev4rt@gmail.com>
2024-02-28 03:18:01 +0700

    Update WeddingEvents.tsx

--- app/components/WeddingEvents.tsx
+++ app/components/WeddingEvents.tsx
@@ -93,6 +93,45 @@ function WeddingEvents() {
               </div>
             </div>
           </RevealWrapper>
+          <RevealWrapper duration={1500} origin="bottom">
+            <div className="mt-10">
+              <img
+                src="/images/weddingEvents/akad5.jpg"
+                alt="akad"
+                className="w-full h-72 object-cover rounded-t-[1.25rem]"
+              />
+              <div className="flex">
+                <div className="w-[20%] bg-[#424242] rounded-bl-[1.25rem] flex items-center">
+                  <p className="rotate-90 text-center text-white font-bold text-[1.75rem] -translate-x-5 lg:-translate-x-3.5 tracking-[5px]">
+                  </p>
+                </div>
+                <div className="w-[80%] bg-white px-4 py-6 rounded-br-[1.25rem] flex flex-col gap-5 items-baseline">
+                  <h1 className="italic text-xl font-light">
+                    AKAD & RESEPSI PERNIKAHAN AKHMAD MUHTAROM & ULIN NIKMAH
+                  </h1>
+                  <hr className="border border-[#5a5a5a80] w-full" />
+                  <p className="flex items-center text-[#5a5a5a] font-light text-sm gap-1.5">
+                  </p>
+                  <p className="text-[0.785rem] text-[#5a5a5a] font-light leading-[1.9]">
+                    <strong className="text-black font-bold">
+                    </strong>
+                  </p>
+                  <a
+                    href="https://facebook.com/RTPROJEX/live_videos"
+                    className="text-[0.75rem] text-[#424242] border border-[#424242] px-[0.4375rem] py-1.5 rounded-full hover:scale-90 ease-linear duration-[0.2s]"
+                  >
+                    FACEBOOK
+                  </a>
+                  <a
+                    href="https://youtube.com/@RTPROJEX"
+                    className="text-[0.75rem] text-[#424242] border border-[#424242] px-[0.4375rem] py-1.5 rounded-full hover:scale-90 ease-linear duration-[0.2s]"
+                  >
+                    YOUTUBE
+                  </a>
+                </div>
+              </div>
+            </div>
+          </RevealWrapper>
         </div>
       </div>
     </section>
