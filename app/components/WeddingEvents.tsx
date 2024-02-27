d76e580711d2d3bd147da24f8bccf886b08d3c53
Romireverend <rev4rt@gmail.com>
2024-02-28 02:33:58 +0700

    Update WeddingEvents.tsx
    gfd

--- app/components/WeddingEvents.tsx
+++ app/components/WeddingEvents.tsx
@@ -19,6 +19,10 @@ function WeddingEvents() {
                 className="w-full h-72 object-cover rounded-t-[1.25rem]"
               />
               <div className="flex">
+                <div className="w-[20%] bg-[#424242] rounded-bl-[1.25rem] flex items-center">
+                  <p className="rotate-90 text-center text-white font-bold text-[1.75rem] -translate-x-5 lg:-translate-x-3.5 tracking-[5px]">
+                  </p>
+                </div>
                 <div className="w-[80%] bg-white px-4 py-6 rounded-br-[1.25rem] flex flex-col gap-5 items-baseline">
                   <h1 className="italic text-xl font-light">
                     Sunday, 21 April 2024 M
@@ -81,6 +85,10 @@ function WeddingEvents() {
                   >
                     GOOGLE MAPS
                   </a>
+                </div>
+                <div className="w-[20%] bg-[#424242] rounded-br-[1.25rem] flex items-center">
+                  <p className="-rotate-90 text-center text-white font-bold text-[1.75rem] -translate-x-[3rem] lg:-translate-x-[2.35rem] tracking-[5px]">
+                  </p>
                 </div>
               </div>
             </div>
