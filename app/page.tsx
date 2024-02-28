1245dbda1b692304e46d484753f800d2959f7a90
romireverends <161525775+romireverends@users.noreply.github.com>
2024-02-29 02:12:59 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,8 @@ import WeddingEvents from "./components/WeddingEvents";
 import Gallery from "./components/Gallery";
 import Footer from "./components/Footer";
 import { RevealWrapper } from "next-reveal";
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
   const [currentOverflow, setCurrentOverflow] = useState("hidden");
