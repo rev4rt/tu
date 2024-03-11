db1763b09f317186e8f955d9c234fa5fef397022
romireverends <161525775+romireverends@users.noreply.github.com>
2024-03-12 02:44:16 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import { Merriweather } from "next/font/google";
-import { SpeedInsights } from "@vercel/speed-insights/next"
-import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 const merriweather = Merriweather({
   weight: ["300", "400", "700", "900"],
