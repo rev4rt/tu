8ab173994e0169c155d8e1cc8fee0a94d7d8fb88
Romireverend <rev4rt@gmail.com>
2024-03-23 02:40:49 +0700

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,18 +5,20 @@ function Footer() {
   return (
     <section id="footer">
       <div className="h-screen bg-[url('/images/footer/bg3.jpeg')] bg-cover bg-[47.5%] bg-no-repeat flex items-end">
+        <div className="flex flex-col items-center text-center gap-4 text-[#4242427A] p-6 z-10 relative">
         <RevealWrapper duration={1500} origin="top">
-              <p className="text-[0.75rem] text-center text-[#4242427A] font-light leading-relaxed">
+              <p className="text-[0.75rem] font-light leading-relaxed">
                 Merupakan suatu kehormatan dan kebahagiaan bagi kami apabila
                 Bapak/Ibu/Saudara/(i) berkenan hadir dan memberikan doa restu
                 kepada kami. Atas kehadiran dan doanya kami mengucapkan
                 terimakasih
               </p>
               <br />
-              <p className="text-[0.75rem] text-center text-[#4242427A] font-light leading-relaxed">
+              <p className="text-[0.75rem] font-light leading-relaxed">
                 وَالسَّلاَمُ عَلَيْكُمْ وَرَحْمَةُ اللهِ وَبَرَكَاتُهُ
               </p>
             </RevealWrapper>
+          </div>
         <div className="relative">
           <div className="bg-[linear-gradient(180deg,#e2cdbc00_00%,#e2cdbc_70%)] absolute inset-x-0 bottom-0 -top-24"></div>
           <div className="flex flex-col items-center text-center gap-4 text-white p-6 z-10 relative">
