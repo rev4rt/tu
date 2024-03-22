e51ae444ddf9dd81223f17c0f9a5dd522d8d7ef0
Romireverend <rev4rt@gmail.com>
2024-03-23 02:32:54 +0700

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -6,7 +6,7 @@ function Footer() {
     <section id="footer">
       <div className="h-screen bg-[url('/images/footer/bg2.jpeg')] bg-cover bg-[47.5%] bg-no-repeat flex items-end">
         <div className="relative">
-          <div className="bg-[linear-gradient(180deg,#ffffff00_00%,#e2cdbc_70%)] absolute inset-x-0 bottom-0 -top-24"></div>
+          <div className="bg-[linear-gradient(180deg,#e2cdbc00_00%,#e2cdbc_70%)] absolute inset-x-0 bottom-0 -top-24"></div>
           <div className="flex flex-col items-center text-center gap-4 text-white p-6 z-10 relative">
             <RevealWrapper duration={1500} origin="top">
               <p className="text-[0.75rem] text-[#4242427A] font-light leading-relaxed">
