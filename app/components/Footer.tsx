175b24977997c7d5b7a9d621f365594fe68c8b97
Romireverend <rev4rt@gmail.com>
2024-03-23 01:02:59 +0700

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -29,10 +29,10 @@ function Footer() {
       </div>
       <div className="bg-[#e2cdbc] p-20 flex items-center justify-center">
         <a href="#"><img src="/images/footer/logo-final.png" alt="logo" className="w-24" /></a>
-      </div>
               <p className="text-[0.75rem] text-center text-white font-light leading-relaxed">
                Copyright © {new Date().getFullYear()} <a href="#">RTPROJEX</a>
               </p>
+        </div>
     </section>
   );
 }
