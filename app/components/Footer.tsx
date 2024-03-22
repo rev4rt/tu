20c9f06ddef40c5b1d510f930d0307d346dc1523
Romireverend <rev4rt@gmail.com>
2024-03-23 03:28:13 +0700

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -7,8 +7,12 @@ function Footer() {
       <div className="h-screen bg-[url('/images/footer/bg3.jpeg')] bg-cover bg-[47.5%] bg-no-repeat flex items-end">
         <div className="relative">
         <div className="flex flex-col items-center text-center gap-4 text-[#4242427A] p-6 z-10 relative">
-          <RevealWrapper duration={4000} origin="bottom">
-                <p className="text-[0.75rem] font-light leading-relaxed">
+        <div className="bg-[#e2cdbc21] rounded-tr-[3.125rem] rounded-bl-[3.125rem] absolute inset-x-4 inset-y-0 z-10"
+            style={{ backdropFilter: "blur(6px)" }}
+          />
+          <div className="relative z-20">
+            <RevealWrapper duration={1500} origin="right">
+              <p className="text-[0.75rem] font-light leading-relaxed">
                 Merupakan suatu kehormatan dan kebahagiaan bagi kami apabila
                 Bapak/Ibu/Saudara/(i) berkenan hadir dan memberikan doa restu
                 kepada kami. Atas kehadiran dan doanya kami mengucapkan
@@ -17,8 +21,10 @@ function Footer() {
               <br />
               <p className="text-[0.75rem] font-light leading-relaxed">
                 وَالسَّلاَمُ عَلَيْكُمْ وَرَحْمَةُ اللهِ وَبَرَكَاتُهُ
-              </p>
+                </p>
+              </div>
             </RevealWrapper>
+          </div>
           </div>
           <div className="bg-[linear-gradient(180deg,#e2cdbc00_00%,#e2cdbc_70%)] absolute inset-x-0 bottom-0 -top-24"></div>
           <div className="flex flex-col items-center text-center gap-4 text-white p-6 z-10 relative">
