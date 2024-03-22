d4fd41a531a16a6f82442da323cc0f21e24698b9
Romireverend <rev4rt@gmail.com>
2024-03-23 04:54:01 +0700

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -14,25 +14,15 @@ function Footer() {
           />
           <div className="relative z-20">
             <RevealWrapper duration={1500} origin="right">
-              <p className="text-sm font-light">WEDDING INVITATION</p>
-              <h1 className="text-3xl font-light mt-2 mb-4">Akhmad Muhtarom<br />&<br />Ulin Nikmah</h1>
+              <p className="text-sm text-center font-light">Merupakan suatu kehormatan dan kebahagiaan bagi kami apabila
+                Bapak/Ibu/Saudara/(i) berkenan hadir dan memberikan doa restu
+                kepada kami. Atas kehadiran dan doanya kami mengucapkan
+                terimakasih.</p>
+              <h1 className="text-3xl text-center font-light mt-2 mb-4">وَالسَّلاَمُ عَلَيْكُمْ وَرَحْمَةُ اللهِ وَبَرَكَاتُهُ</h1>
             </RevealWrapper>
           </div>
           </div>
           <div className="flex flex-col items-center text-center gap-4 text-white p-6 z-10 relative">
-            <RevealWrapper duration={1500} origin="top">
-              <p className="text-[0.75rem] font-light leading-relaxed">
-                Merupakan suatu kehormatan dan kebahagiaan bagi kami apabila
-                Bapak/Ibu/Saudara/(i) berkenan hadir dan memberikan doa restu
-                kepada kami. Atas kehadiran dan doanya kami mengucapkan
-                terimakasih
-              </p>
-            </RevealWrapper>
-            <RevealWrapper duration={1500} origin="top">
-              <p className="text-[0.75rem] font-light leading-relaxed">
-                وَالسَّلاَمُ عَلَيْكُمْ وَرَحْمَةُ اللهِ وَبَرَكَاتُهُ
-              </p>
-            </RevealWrapper>
             <br />
             <br />
             <RevealWrapper duration={1500} origin="bottom">
@@ -42,7 +32,7 @@ function Footer() {
         </div>
       </div>
       <div className="bg-[#9C76C2] p-20 flex items-center justify-center">
-        <img src="/images/footer/lf.png" alt="logo" className="w-24" />
+        <img src="/images/footer/logo-final.png" alt="logo" className="w-24" />
       </div>
     </section>
   );
