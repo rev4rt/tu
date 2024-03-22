994e93616f6309e60317001371c07386c57cf2e4
Romireverend <rev4rt@gmail.com>
2024-03-23 02:19:15 +0700

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,11 +4,7 @@ import { RevealWrapper } from "next-reveal";
 function Footer() {
   return (
     <section id="footer">
-      <div className="h-screen bg-[url('/images/footer/bg2.jpeg')] bg-cover bg-[47.5%] bg-no-repeat flex items-end">
-        <div className="relative">
-          <div className="bg-[linear-gradient(180deg,#ffffff00_00%,#e2cdbc_70%)] absolute inset-x-0 bottom-0 -top-24"></div>
-          <div className="flex flex-col items-center text-center gap-4 text-white p-6 z-10 relative">
-            <RevealWrapper duration={1500} origin="top">
+                  <RevealWrapper duration={1500} origin="top">
               <p className="text-[0.75rem] font-light leading-relaxed">
                 Merupakan suatu kehormatan dan kebahagiaan bagi kami apabila
                 Bapak/Ibu/Saudara/(i) berkenan hadir dan memberikan doa restu
@@ -21,6 +17,10 @@ function Footer() {
                 وَالسَّلاَمُ عَلَيْكُمْ وَرَحْمَةُ اللهِ وَبَرَكَاتُهُ
               </p>
             </RevealWrapper>
+      <div className="h-screen bg-[url('/images/footer/bg2.jpeg')] bg-cover bg-[47.5%] bg-no-repeat flex items-end">
+        <div className="relative">
+          <div className="bg-[linear-gradient(180deg,#ffffff00_00%,#e2cdbc_70%)] absolute inset-x-0 bottom-0 -top-24"></div>
+          <div className="flex flex-col items-center text-center gap-4 text-white p-6 z-10 relative">
             <br />
             <br />
             <RevealWrapper duration={1500} origin="bottom">
