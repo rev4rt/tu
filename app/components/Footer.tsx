271a8f0d3cd322ea4c3496dbc5cf370385d6dfcc
Romireverend <rev4rt@gmail.com>
2024-03-23 03:40:59 +0700

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -23,6 +23,7 @@ function Footer() {
                 </p>
                </div>
             </RevealWrapper>
+          </div>
           <div className="bg-[linear-gradient(180deg,#e2cdbc00_00%,#e2cdbc_70%)] absolute inset-x-0 bottom-0 -top-24"></div>
           <div className="flex flex-col items-center text-center gap-4 text-white p-6 z-10 relative">
             <br />
