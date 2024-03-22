3a549ff800c09a0e53b5baa0bfa68848eb349bef
Romireverend <rev4rt@gmail.com>
2024-03-23 04:47:33 +0700

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,9 +4,21 @@ import { RevealWrapper } from "next-reveal";
 function Footer() {
   return (
     <section id="footer">
-      <div className="h-screen bg-[url('/images/gallery/bg2.jpg')] bg-cover bg-[47.5%] bg-no-repeat flex items-end">
+      <div className="h-screen bg-[url('/images/footer/bg3.jpeg')] bg-cover bg-[47.5%] bg-no-repeat flex items-end">
         <div className="relative">
           <div className="bg-[linear-gradient(180deg,#ffffff00_0%,#9C76C2_67%)] absolute inset-x-0 bottom-0 -top-24"></div>
+                  <div className="absolute inset-0 bg-black/30" />
+        <div className="relative py-8 px-10 w-full">
+          <div
+            className="bg-[#e2cdbc21] rounded-tr-[3.125rem] rounded-bl-[3.125rem] absolute inset-x-4 inset-y-0 z-10"
+            style={{ backdropFilter: "blur(6px)" }}
+          />
+          <div className="relative z-20">
+            <RevealWrapper duration={1500} origin="right">
+              <p className="text-sm font-light">WEDDING INVITATION</p>
+              <h1 className="text-3xl font-light mt-2 mb-4">Akhmad Muhtarom<br />&<br />Ulin Nikmah</h1>
+            </RevealWrapper>
+          </div>
           <div className="flex flex-col items-center text-center gap-4 text-white p-6 z-10 relative">
             <RevealWrapper duration={1500} origin="top">
               <p className="text-[0.75rem] font-light leading-relaxed">
