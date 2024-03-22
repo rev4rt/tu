588acd32c8f05bedefd09eb5b92e121f062936a3
Romireverend <rev4rt@gmail.com>
2024-03-23 01:15:28 +0700

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -7,13 +7,13 @@ function Footer() {
       <div className="h-screen bg-[url('/images/footer/bg3.jpeg')] bg-cover bg-[47.5%] bg-no-repeat flex items-end">
         <div className="relative">
           <div className="bg-[linear-gradient(180deg,#FFFFFF00_0%,#e2cdbc_67%)] absolute inset-x-0 bottom-0 -top-24"></div>
+          <div className="flex flex-col items-center text-center gap-4 text-[#101010] p-6 z-10 relative">
+            <RevealWrapper duration={1500} origin="top">
           <div className="bg-[#ffffff21] rounded-lg-[3.125rem]"
             style={{ backdropFilter: "blur(6px)" }}>Merupakan suatu kehormatan dan kebahagiaan bagi kami apabila
                 Bapak/Ibu/Saudara/(i) berkenan hadir dan memberikan doa restu
                 kepada kami. Atas kehadiran dan doanya kami mengucapkan
                 terimakasih.</div>
-          <div className="flex flex-col items-center text-center gap-4 text-[#101010] p-6 z-10 relative">
-            <RevealWrapper duration={1500} origin="top">
               <p className="text-[0.75rem] font-light leading-relaxed">
                 وَالسَّلاَمُ عَلَيْكُمْ وَرَحْمَةُ اللهِ وَبَرَكَاتُهُ</p>
            </RevealWrapper>
