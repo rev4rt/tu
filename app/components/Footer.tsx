7abb1a0d3892607e387d160d85efa034d2361c38
Romireverend <rev4rt@gmail.com>
2024-03-23 00:59:15 +0700

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -8,7 +8,7 @@ function Footer() {
         <div className="relative">
           <div className="bg-[linear-gradient(180deg,#FFFFFF00_0%,#e2cdbc_67%)] absolute inset-x-0 bottom-0 -top-24"></div>
           <div className="bg-[#ffffff21] rounded-lg-[3.125rem]"
-            style={{ backdropFilter: "blur(6px)" }}>
+            style={{ backdropFilter: "blur(6px)" }}/>
           <div className="flex flex-col items-center text-center gap-4 text-[#101010] p-6 z-10 relative">
             <RevealWrapper duration={1500} origin="top">
               <p className="text-[0.75rem] font-light leading-relaxed">
@@ -19,12 +19,12 @@ function Footer() {
               </p>
               <p>وَالسَّلاَمُ عَلَيْكُمْ وَرَحْمَةُ اللهِ وَبَرَكَاتُهُ</p>
            </RevealWrapper>
-            </div>
             <br />
             <br />
             <RevealWrapper duration={1500} origin="bottom">
               <h1 className="text-4xl font-light mt-2">Akhmad Muhtarom<br />&<br />Ulin Nikmah</h1>
             </RevealWrapper>
+            </div>
           </div>
         </div>
       </div>
