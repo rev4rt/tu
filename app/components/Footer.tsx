86a64992fd08a20c9a7446abcd1962bb5c859b1f
Romireverend <rev4rt@gmail.com>
2024-03-23 04:18:37 +0700

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -7,26 +7,27 @@ function Footer() {
       <div className="h-screen bg-[url('/images/footer/bg3.jpeg')] bg-cover bg-[47.5%] bg-no-repeat flex items-end">
         <div className="relative">
           <div className="bg-[linear-gradient(180deg,#ffffff00_0%,#e2cdbc_67%)] absolute inset-x-0 bottom-0 -top-24"></div>
-          <div className="flex flex-col items-center text-center gap-4 text-white p-6 z-10 relative">
-        <div className="absolute inset-0 bg-black/30" />
-        <div className="relative py-8 px-10 w-full">
-          <div
-            className="bg-[#9C76C222] rounded-tr-[3.125rem] rounded-bl-[3.125rem] absolute inset-x-4 inset-y-0 z-10"
-            style={{ backdropFilter: "blur(6px)" }}
-          />
-          <div className="relative z-20">
-            <RevealWrapper duration={1500} origin="right">
-              <p className="text-sm font-light">WEDDING INVITATION</p>
-              <h1 className="text-3xl font-light mt-2 mb-4">Lulu & Farhan</h1>
-              <div className="grid grid-cols-2 border-y border-y-white text-center text-[0.75rem] font-light">
-                <p className="leading-[1.85] py-2 px-2 border-r border-r-white flex flex-col gap-2">
-                  <span>SAVE THE DATE</span>
-                  <span>Saturday, 13 April 2024</span>
-                </p>
+           <div className="flex flex-col items-center text-center gap-4 text-white p-6 z-10 relative">
+            <div className="absolute inset-0 bg-black/30" />
+             <div className="relative py-8 px-10 w-full">
+              <div
+                className="bg-[#9C76C222] rounded-tr-[3.125rem] rounded-bl-[3.125rem] absolute inset-x-4 inset-y-0 z-10"
+                style={{ backdropFilter: "blur(6px)" }}
+               />
+                <div className="relative z-20">
+                 <RevealWrapper duration={1500} origin="right">
+                  <p className="text-sm font-light">WEDDING INVITATION</p>
+                  <h1 className="text-3xl font-light mt-2 mb-4">Lulu & Farhan</h1>
+                   <div className="grid grid-cols-2 border-y border-y-white text-center text-[0.75rem] font-light">
+                    <p className="leading-[1.85] py-2 px-2 border-r border-r-white flex flex-col gap-2">
+                     <span>SAVE THE DATE</span>
+                     <span>Saturday, 13 April 2024</span>
+                     </p>
               </div>
             </RevealWrapper>
           </div>
         </div>
+             </div>
             <br />
             <br />
             <RevealWrapper duration={1500} origin="bottom">
