1783001724431a67027f22fd089450a00fd6b064
Romireverend <rev4rt@gmail.com>
2024-03-31 23:07:28 +0700

    Update Hero.tsx

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import React from "react";
 import { RxEnvelopeOpen } from "react-icons/rx";
 import useSound from "use-sound";
@@ -30,11 +29,10 @@ function Hero({
             <p className="text-[0.8rem]">Kepada Bapak/Ibu/Saudara/(i)</p>
             <p className="text-[0.8rem]">Di Tempat</p>
             <RevealWrapper duration={4000} origin="bottom">
-              <a
-                href="#countdown"
+              <a href="#countdown"
                 onClick={() => {
                   setCurrentOverflow("auto");
-                  play("auto");
+                  play();
                 }}
                 className="font-bold text-sm bg-[#e2cdbc36] border border-[#bdb08f8c] rounded-lg flex items-center gap-2 px-6 py-3 mt-6 hover:scale-90 ease-linear duration-[0.2s]"
               >
